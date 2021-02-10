2f50d14d7bc172a464797f9dde8c97541841353d
백주엽 <wnduq08217@naver.com>
2021-02-10 18:12:10 +0900

    카카오 기출 2/10

--- 자물쇠와열쇠.py
+++ 자물쇠와열쇠.py
@@ -1,5 +1,5 @@
 def solution(key, lock):
-    answer = True
+    answer = False
     # 키를 돌리고 이동 시켜서
     # lock의 홈에 맞추어여함
 
@@ -24,29 +24,45 @@ def solution(key, lock):
     def rotate(x, y, degree, n=N):
 
         if degree == 90:
-            x, y = y, n-x
+            x, y = y, n-1-x
         elif degree == 180:
             x, y = y, x
         elif degree == 270:
-            x, y = n-y, x
+            x, y = n-1-y, x
         else:
             return x, y
 
         return x, y
 
+
     for degree in [90,180,270,360]:
         for v in range(-(N-1), M):
             for h in range(-(N-1), M):
+                holes = len(hole_axis)
+                rest_keys = []
                 for axis in key_axis:
                     x, y = axis
                     # 회전이동
                     x_new, y_new = rotate(x, y, degree)
                     # 0 ~ n-m 좌우상하로 키좌표이동
-                    x_new, y_new = x_new + v, y_new + h
+                    x_new, y_new = x_new + h, y_new + v
                     # 홈과 키좌표 다같고
-                    hole_axis
-                    # 남는키가 범위내에 있다면
-                    # 트루
+                    if (x_new, y_new) in hole_axis:
+                        holes-=1
+                    else:
+                        rest_keys.append((x_new, y_new))
+                # 키가 다있으면
+                if holes == 0 :
+                    # 남는키가 없거나 전부 범위밖이면
+                    if rest_keys == []:
+                        return True
+                    for rest_key in rest_keys:
+                        x, y = rest_key
+                        # 안맞는 키있으면 다음 탐색
+                        if 0<=x<M and 0<=y<M:
+                            break
+                        return True
 
     return answer
 
+solution([[0, 0, 0], [1, 0, 0], [0, 1, 1]], [[1, 1, 1], [1, 1, 0], [1, 0, 1]])
